0dec3cdf665d1dadf9be84968b6344f272d6fa72
Christian <c.jr.camelot@gmail.com>
2021-02-11 16:25:06 +0100

    ajout d'information et passage en celsius

--- src/components/conditions/conditions.js
+++ src/components/conditions/conditions.js
@@ -5,8 +5,12 @@ const conditions = (props) => {
        <div>
            {props.responseObj.cod === 200 ?
                <div>
-                   <p><strong>{props.responseObj.name}</strong></p>
-                   <p>It is currently {Math.round(props.responseObj.main.temp)} degrees K out with {props.responseObj.weather[0].description}.</p>
+                   <h2><strong>{props.responseObj.name}</strong></h2>
+                   <p>II fait actuellement {Math.round(props.responseObj.main.temp - 273.15)} °C avec {props.responseObj.weather[0].description}.</p>
+                   <p>temp Min: {Math.round(props.responseObj.main.temp_min - 273.15)} °C</p>
+                   <p>temp Max: {Math.round(props.responseObj.main.temp_max - 273.15)} °C</p>
+                   <p>humidité: {props.responseObj.main.humidity} %</p>
+
                </div>
            : null
            }
