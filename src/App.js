8744eabcaa1fbe59737e5c458f21d381a8616695
Christian <c.jr.camelot@gmail.com>
2021-02-11 16:33:36 +0100

    ajout d'un logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Prévision from "./components/prévisions/prévisions";
+import Logo from './components/logo/logo';
 
 function App() {
  return (
    <div className="App">
      <header className="App-header">
-       <h1>Météo</h1>
+     <Logo />
+      <h1>Météo</h1>
      </header>
      <main>
        <Prévision />
